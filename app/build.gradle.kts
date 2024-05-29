d00e5a33dceba071fde8c0beb15788bb00acd6b5
srnb1 <hanisky1@naver.com>
2024-05-29 20:14:21 +0900

    default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("com.google.android.gms:play-services-location:19.0.1")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
 }
