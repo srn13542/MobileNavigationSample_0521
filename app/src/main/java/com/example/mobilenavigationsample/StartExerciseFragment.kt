5e8767bf55133254a15d5f28b22dd9b9d5c7bafe
srnb1 <hanisky1@naver.com>
2024-05-31 10:21:51 +0900

    default

--- app/src/main/java/com/example/mobilenavigationsample/StartExerciseFragment.kt
+++ app/src/main/java/com/example/mobilenavigationsample/StartExerciseFragment.kt
@@ -1,12 +1,17 @@
 package com.example.mobilenavigationsample
 
-
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.mobilenavigationsample.databinding.ActivityNaviBinding
 import com.example.mobilenavigationsample.databinding.FragmentStartExerciseBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -16,11 +21,15 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-
-
-
-
-
+import androidx.fragment.app.FragmentManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import android.os.Looper
+import android.util.Log
+import com.google.android.gms.location.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -29,18 +38,32 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private lateinit var binding: FragmentStartExerciseBinding // 주석 하기
 
-
-
 /**
  * A simple [Fragment] subclass.
  * Use the [StartExerciseFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class StartExerciseFragment : Fragment() {
+class StartExerciseFragment(val activity: Activity) : Fragment()/*, OnMapReadyCallback*/ {
+//class MapsFragment(val activity: Activity) : Fragment(), OnMapReadyCallback
+    //onMapReadyCallBack ==
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    //사용자의 위치를 받기 위한 코드
+    lateinit var locationPermission: ActivityResultLauncher<Array<String>>
+    private lateinit var mMap: GoogleMap
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationCallback: LocationCallback
+
+    /*  private val callback = OnMapReadyCallback { googleMap ->
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }*/
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,67 +73,45 @@ class StartExerciseFragment : Fragment() {
         }
     }
 
+    private lateinit var mView: MapView
+
+    //MapView 나중에 추가.
+
     override fun onCreateView(
 
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
-        binding = FragmentStartExerciseBinding.inflate(layoutInflater)
-        // Inflate the layout for this fragment
-        //여기에
-        //여기부터 주석 시작
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.startExerciseMap) as? SupportMapFragment
-        mapFragment?.getMapAsync{ googleMap->
-            //초기 장소 설정
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(37.3184153,126.7064638),16F))
-            // 최소 줌
-            googleMap.setMinZoomPreference(14F)
+        // Inflate the layout for this fragment*/
 
-            val jsonString = this@StartExerciseFragment.assets.open("Location.json").bufferedReader().readText()
-            val jsonType = object: TypeToken<Maps>(){}.type
-            val maps = Gson().fromJson(jsonString,jsonType) as Maps // CameraMap
-            //cameraMap == maps
-            //Camera == Location_information
-            maps.map.forEach{
-                    map -> // camera
-                val icon = when(map.town){
-                    "정왕동" -> BitmapDescriptorFactory.fromResource(R.drawable.nav_exercise_show)
-                    else -> BitmapDescriptorFactory.fromResource(R.drawable.nav_exercise_show)
-                }
-                googleMap.addMarker(
-                    MarkerOptions()
-                        .icon(icon)
-                        .position(LatLng(map.latitude,map.longitude))
-                        .title(map.town)
-                        .snippet("${map.latitude},${map.longitude}")
-                )?.let{
-                    it.tag = map
-                }
-
-
-            }
-
-            // 클릭시 상세 정보
-            googleMap.setOnInfoWindowClickListener {
-                val map = it.tag as Location_information
-                val intent = Intent (this@StartExerciseFragment, MapActivity::class.java).apply{
-                    putExtra("town", map.town)
-                    putExtra("name", map.name)
-                    putExtra("address", map.address)
-                    putExtra("latitude",  map.latitude)
-                    putExtra("longitude", map.longitude)
-                    putExtra("exercise", map.exercise)
-                }
-                this@StartExerciseFragment.startActivity(intent)
+        locationPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { results ->
+            if (!results.all { it.value }) {
+                Toast.makeText(activity, "권한 승인이 필요합니다.", Toast.LENGTH_LONG).show()
             }
         }
-// 여기까지 주석하기
 
-
+        //권한 요청
+        locationPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
 
         return inflater.inflate(R.layout.fragment_start_exercise, container, false)
     }
+
+/*  오류 나는 부분이라 임시 처리,
+ override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+*/
+
 
     companion object {
         /**
@@ -122,6 +123,7 @@ class StartExerciseFragment : Fragment() {
          * @return A new instance of fragment StartExerciseFragment.
          */
         // TODO: Rename and change types and number of parameters
+        /* 오류 나는 부분이라 임시 주석 처리, 0531
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             StartExerciseFragment().apply {
@@ -129,9 +131,8 @@ class StartExerciseFragment : Fragment() {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
-            }
+            }*/
     }
 }
 
 
-}
